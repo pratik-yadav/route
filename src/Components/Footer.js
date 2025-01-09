58293c10f966c6c321735c7ea766aff3badeb150
Prateek Yadav <y.prateek.142@gmail.com>
2025-01-09 17:41:44 +0530

    new branch chnage

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -15,9 +15,10 @@ export default function Footer() {
                             />
                         </Link>
                     </div>
+
                     <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
                         <div>
-                            <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">Resources</h2>
+                            <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">Re</h2>
                             <ul className="text-gray-500 font-medium">
                                 <li className="mb-4">
                                     <Link to="/" className="hover:underline">
